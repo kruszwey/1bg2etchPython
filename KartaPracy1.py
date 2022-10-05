0f7aaf5f6fce212796181cf869dac9b53f6155e9
1b30WW <114579287+kruszwey@users.noreply.github.com>
2022-10-05 10:55:23 +0000

    zrobione zadanie 5-7

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -13,10 +13,24 @@
 # b = int(input())
 # print((a-b)**3)
 
-# zad 4
+#zad 4
 # a = int(input())
 # b = int(input())
 # c = int(input())
 # print(a*b*c)
 
-# zad 5
+#zad 5
+# a = int(input())
+# b = int(input())
+# print(2*(a+b)/5)
+# print((a+b)*0,4)
+
+#zad 6
+
+# brutto = int(input())
+# print(brutto/ 1.23)
+
+#zad 7
+a = int(input())
+b = int(input())
+print(a % b)
