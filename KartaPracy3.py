15c306c5ad528e3e65446fa882cdb525f4ead1b6
1b30WW <114579287+kruszwey@users.noreply.github.com>
2022-10-12 11:20:41 +0000

    wykonano zadanie 2 i jeszcze jakieś z pętlą FOR

---
+++ KartaPracy3.py
@@ -0,0 +1,20 @@
+# pentla liczb dwucyfrowych od 10 do 21
+# for i in range(10,22): print(i, end=" ")
+# Pentla liczb dwucyfrowych nieparzystych od 15 do 31
+# for i in range(15,32,2): print(i, end=" ")
+# Pentla liczb dwucyfrowych malejąco (step ujemny)
+# for i in range(99,9,-1): print(i, end=" ")
+# pentla liczb dwucyfrowych malejących(step dodatni)
+# for i in range(10,100,1): print(109-i, end=" ")
+# ZADANKO 5
+# for i in range(100,1000,20): print(i, end=" ")
+
+
+# Zad 1
+# n = int(input())
+# for i in range(n):
+#   print (i**3+3, end=" ")
+
+# Zad 2
+# for i in range(105,1000,15) :
+#   print(i, end=" ")
