0991f6988511934e41cb5832eb69531900b7cba4
1b30WW <114579287+kruszwey@users.noreply.github.com>
2022-10-19 11:20:20 +0000

    zadanka

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -20,11 +20,23 @@
 #   print(i, end=" ") 
 
 # Zad 3
-# n int(input())
+# n = int(input())
 # for i in range(1, n+1):
 #   if n % i == 0:
-#     print(i)
+#     print(i, end= ' ')
+
+# Zad 4
+# suma = 0
+# for i in range(10, 100):
+#   suma = suma + i
+# print(suma)
+
+#(TRUDNE) Zad 5
+for i in range(5):
+  
+
 # PRE 2
+
 
 # Pętle for liczb trzycyfrowych podzielnych przez 13
 # for i in range(104,999,13):
@@ -35,3 +47,10 @@
 # Pętle for potęg cyfr: 0, 1, 4, 9, 16 .. 81 
 # for i in range(0, 10):
 #   print(i**2)
+
+# DOD - suma liczb trzycyfrowych parzystych
+# suma = 0
+# for i in range(100,1000, 2):
+#   suma = suma + i
+# print(suma)
+
